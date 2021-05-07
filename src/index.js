8510b172c5e1af1063cbdf3ec44cd8f47e1d98b8
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-05-07 20:57:49 +0700

    objects, array, arguments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,66 @@
 const {ApolloServer, gql} = require('apollo-server');
 
+// type checking
+// String! => type string not null
+// query vs mutation
+// objects
+// array
+// arguments
+
+// crud
+
 const typeDefs = gql`
 
 type Query{
     hello: String!
 }
 
+type User{
+    id: ID!
+    username: String!
+}
+
+type Error {
+    field: String!
+    message: String!
+}
+
+type RegisterResponse{
+    errors: [Error]
+    # errors: [Error!]! jika ga mau nge return null pada type error dan field errors
+    user: User
+}
+
+input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+}
+
+type Mutation{
+    register(userInfo: UserInfo): RegisterResponse!
+    login(userInfo: UserInfo): Boolean!
+}
+
 `;
 
 const resolvers = {
     Query: {
-        hello: () => 'hello world'
+        hello: () => 'hello world!'
+    },
+    Mutation: {
+        login: () => true,
+        register: () =>({
+            errors: [{
+                field: 'username',
+                message: 'bad'
+            }],
+            user: {
+            id: 1,
+            username: "bob"
+            }
+            
+        })
     }
 }
 
